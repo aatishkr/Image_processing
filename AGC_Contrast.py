c8e5d9635671e0c24b74342a7ee5dffbd707a03e
aatishkr <kumar.aatish2@gmail.com>
2021-11-23 15:27:05 +0530

    HE added

--- AGC_Contrast.py
+++ AGC_Contrast.py
@@ -52,10 +52,27 @@ def AGC_contrast(img):
             img_new[i, j] = LUT[img[i, j]]
     return img_new
 
+def histogram_equilizarion(img):
+    hist = histogram(img)
+    CDF  = np.zeros((65536), dtype=np.float64)
+    CDF[0] = hist[0]
+    for i in range(1,65535):
+        CDF[i] = hist[i] + CDF[i-1]
+    for i in range(65535):
+        CDF[i] = CDF[i]/76800
+    img_new = np.zeros((240,320), dtype=np.uint8)
+    for i in range(height):
+        for j in range(width):
+            img_new[i, j] = CDF[img[i, j]]*255
+    return img_new
+
 for i in range(len(img_array)):
     img = AGC_contrast(img_array[i])
+    img_1 = histogram_equilizarion(img_array[i])
     # cv2.imshow("grayscale image",img_array[i])
-    cv2.imshow("grayscale image" ,img)
+    cv2.imshow("AGC" ,img)
+    cv2.imshow("HE" ,img_1)
+    cv2.imshow("Original" ,img_array[i])
     print(i)
     # hist = histogram(img_array[i])
     # plt.plot(hist)
