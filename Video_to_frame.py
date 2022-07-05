2126eb66da081556ddf2d159d750bf37f9867ec8
aatishkr <kumar.aatish2@gmail.com>
2022-07-05 21:31:50 +0530

    video_to_frame

--- Video_to_frame.py
+++ Video_to_frame.py
@@ -1,8 +1,13 @@
 import cv2
+
 vidcap = cv2.VideoCapture('1630924578.avi')
+
 success,image = vidcap.read()
+
 count = 0
+
 success = True
+
 while success:
   success,image = vidcap.read()
   cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
